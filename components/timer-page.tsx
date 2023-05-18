f02868003db51e22cd2e28313cb11358e5a8a558
Karl <karl.jakelski@gmail.com>
2023-05-18 11:45:12 +0200

    completed verification and ux input for timer

--- components/timer-page.tsx
+++ components/timer-page.tsx
@@ -1,9 +1,53 @@
-import { useRef, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
+
+function TimePicker() {
+    const [userInput, setUserInput] = useState("000000");
+
+    function handleInput(e: KeyboardEvent<HTMLInputElement>, timeUnit: string) {
+        const isNumber = /^[0-9]$/i.test(e.key);
+
+        if (e.key === "Esc" || e.key === "Escape") {
+            setUserInput("000000");
+            return;
+        }
+
+        if (!isNumber) {
+            e.stopPropagation();
+            return;
+        }
+
+        let newInput;
+
+        if (timeUnit === "hours")
+                newInput = userInput.slice(1, 2) + e.key + userInput.slice(2, 6);
+
+        else if (timeUnit === "minutes")
+                newInput = userInput.slice(1, 4) + e.key + userInput.slice(4, 6);
+
+        else 
+                newInput = userInput.slice(-5) + e.key;
+
+        setUserInput(newInput);
+    }
+
+
+    return (
+        <div>
+            <input value={userInput[0] + userInput[1]} className="text-black" type="text" onKeyDown={(e) => handleInput(e, "hours")} />
+            <text>h</text>
+            <input value={userInput[2] + userInput[3]} className="text-black" type="text" onKeyDown={(e) => handleInput(e, "minutes")} />
+            <text>m</text>
+            <input value={userInput[4] + userInput[5]} className="text-black" type="text" onKeyDown={(e) => handleInput(e, "seconds")} />
+            <text>s</text>
+        </div>
+    )
+}
 
 function Timer() {
     const [duration, setDuration] = useState(3000);
-    const [now, setNow] = useState(0); 
+    const [now, setNow] = useState(0);
     const [paused, setPaused] = useState(true);
+
     let endTime = useRef(0);
     let intervalID = useRef<NodeJS.Timer>();
 
@@ -20,21 +64,21 @@ function Timer() {
 
     function endTimer() {
         clearInterval(intervalID.current);
-
     }
 
-    let displayTime = Math.round( (endTime.current - Date.now()) / 10 ) / 100;
+    let displayTime = Math.round((endTime.current - Date.now()) / 10) / 100;
 
     if (displayTime <= 0) {
         displayTime = 0;
         endTimer();
     }
 
-    return(
+    return (
         <div>
             <h1>Timer</h1>
+            <TimePicker />
             <h2>{displayTime.toFixed(2)}</h2>
-            <button onClick={() => startTimer()}>Start</button>
+            <button onClick={startTimer}>Start</button>
         </div>
     )
 }
@@ -44,7 +88,7 @@ export function TimerPage() {
 
     return (
         <div>
-            <Timer/>
+            <Timer />
         </div>
     )
 }
