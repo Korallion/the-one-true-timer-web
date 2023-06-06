ef64b3695d6a4a862f94f68ba93fc4b9c9b21a9b
karl <karl.jakelski@gmail.com>
2023-06-06 22:30:27 +1200

    added useEffect hook to stop infinite setInterval

--- components/clock-page.tsx
+++ components/clock-page.tsx
@@ -1,34 +1,39 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { deleteIdFromArray, addIdToArray } from "@/functions/general";
 
-export function Clock({id, deleteClock}: {id: number, deleteClock: (id: number) => void}) {
+export function Clock({ id, deleteClock }: { id: number, deleteClock: (id: number) => void }) {
     const [date, setDate] = useState<string>();
     const [time, setTime] = useState<string>();
     const [timeZone, setTimeZone] = useState(0);
 
-    setInterval(() => {
-        const currentTime = new Date();
-        setTime(currentTime.toLocaleTimeString());
-        setDate(currentTime.toDateString());
-    }, 10);
+    useEffect(() => {
+        setInterval(() => {
+            const currentTime = Date.now()
+            const timeZoneAddition = timeZone * 60 * 60 * 1000;
+            const currentDate = new Date(Date.now() + timeZone * 60 * 60 * 1000);
+            setTime(currentDate.toLocaleTimeString());
+            setDate(currentDate.toDateString());
+        }, 1000)
+    }, []);
 
     return (
         <div>
             {`Clock ${id}`}
             {`The date is ${date} and the time is ${time}`}
-            <input 
-                className="bg-black border-white" 
-                type="number" 
-                min={-12}  
-                max={14} 
+            <input
+                className="bg-black border-white"
+                type="number"
+                min={-12}
+                max={14}
                 defaultValue={0}
-                onChange={(e) => {setTimeZone(Number(e.target.value))}}/>
+                onChange={(e) => { setTimeZone(Number(e.target.value)) }}
+            />
             <button onClick={() => deleteClock(id)}>Delete</button>
         </div>
     )
 }
 
-export function ClockPage({className}: {className: string}) {
+export function ClockPage({ className }: { className: string }) {
     const [clocks, setClocks] = useState([0]);
 
     const clockComponents = clocks.map((id) => {
