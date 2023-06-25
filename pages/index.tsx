4eb2dd3c9ac2e1c2394d615f39c5f38e8612d930
karl <karl.jakelski@gmail.com>
2023-06-25 19:11:45 +0200

    added responsive css for menu bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,31 +37,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        <h1 className='menu-title'>The One True Timer</h1>
+      <div className='content'>
         <div className='menu-bar'>
-          <button
-            className='menu-button'
-            onClick={() => setActiveTab("Stopwatches")}
-          >Stopwatches</button>
+          <h1 className='menu-title'>The One True Timer</h1>
+          <div className='navigation-container'>
+            <button
+              className={activeTab === "Stopwatches" ? 'menu-button-selected' : 'menu-button'}
+              onClick={() => setActiveTab("Stopwatches")}
+            >Stopwatches</button>
 
-          <button
-            className='menu-button'
-            onClick={() => setActiveTab("Timers")}
-          >Timers</button>
+            <button
+              className={activeTab === "Timers" ? 'menu-button-selected' : 'menu-button'}
+              onClick={() => setActiveTab("Timers")}
+            >Timers</button>
 
-          <button
-            className='menu-button'
-            onClick={() => setActiveTab("Alarms")}
-          >Alarms</button>
+            <button
+              className={activeTab === "Alarms" ? 'menu-button-selected' : 'menu-button'}
+              onClick={() => setActiveTab("Alarms")}
+            >Alarms</button>
 
-          <button
-            className='menu-button'
-            onClick={() => setActiveTab("Clocks")}
-          >Clocks</button>
+            <button
+              className={activeTab === "Clocks" ? 'menu-button-selected' : 'menu-button'}
+              onClick={() => setActiveTab("Clocks")}
+            >Clocks</button>
+          </div>
         </div>
 
-        <div className='content'>
+        <div className='feature-pages'>
           <StopwatchPage className={"" + (pageIndex === 0 ? "" : " hidden")} />
           <TimerPage className={"" + (pageIndex === 1 ? "" : " hidden")} />
           <AlarmPage className={"" + (pageIndex === 2 ? "" : " hidden")} />
