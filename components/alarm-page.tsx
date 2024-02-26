b2912570bef378ea824689fc41442935a39d12d0
Karl <karl.jakelski@gmail.com>
2024-02-26 21:00:42 +0100

    fix: correct setting alarm for earlier in the day than current time chore: clean up code

--- components/alarm-page.tsx
+++ components/alarm-page.tsx
@@ -8,91 +8,89 @@ import { Howl } from 'howler';
 // - repeat alarm / choice of days
 // - "tomorrow" alarm
 // - snooze function
-// - if time is set for before "current" time, set for tomorrow
 
 const alarmSound = new Howl({
-    src: ["/tott_timer_end.mp3"],
+  src: ["/tott_timer_end.mp3"],
 })
 
 function Alarm({ id, deleteAlarm }: { id: number, deleteAlarm: () => void }) {
-    const [endTime, setEndTime] = useState(0);
-    const [remainingTime, setRemainingTime] = useState(0);
-    const [isActive, setIsActive] = useState(false);
+  const [endTime, setEndTime] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-    const intervalID = useRef<NodeJS.Timer>();
-    const buttonText = isActive ? "Deactivate" : "Activate"
+  const intervalID = useRef<NodeJS.Timer>();
+  const buttonText = isActive ? "Deactivate" : "Activate"
 
-    function activateAlarm() {
-        intervalID.current = setInterval(() => {
-            const now = new Date().getTime();
+  function activateAlarm() {
+    intervalID.current = setInterval(() => {
+      const now = new Date().getTime();
 
-            if (endTime - now <= 0) {
-                alarmSound.play();
-                setRemainingTime(0);
-                clearInterval(intervalID.current);
-            } else {
-                setRemainingTime(endTime - now);
-            }
-
-        }, 100);
-        setIsActive(true)
-    }
-
-    function deactivateAlarm() {
-        clearInterval(intervalID.current);
+      if (endTime - now > 0) {
+        setRemainingTime(endTime - now);
+      } else {
+        alarmSound.play();
         setRemainingTime(0);
-        setIsActive(false)
+        clearInterval(intervalID.current);
+      }
+
+    }, 100);
+    setIsActive(true)
+  }
+
+  function deactivateAlarm() {
+    clearInterval(intervalID.current);
+    setRemainingTime(0);
+    setIsActive(false)
+  }
+
+  function toggleAlarmActivation() {
+    if (isActive) {
+      deactivateAlarm()
+    } else {
+      activateAlarm()
+    }
+  }
+
+  function setAlarmTime(input: number) {
+    const today = new Date();
+    const beginningOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    let newEndTime = 0
+
+    if (input <= today.getTime()) {
+      newEndTime = input + beginningOfDay.getTime() + 24 * 60 * 60 * 1000
+    } else {
+      newEndTime = input + beginningOfDay.getTime()
     }
 
-    function toggleAlarmActivation() {
-        if (isActive) {
-            deactivateAlarm()
-        } else {
-            activateAlarm()
-        }
-    }
+    setEndTime(newEndTime)
+  }
 
-    return (
-        <div>
-            {`Alarm ${id}`}
-            <TimePicker
-                setTime={(input: number) => {
-                    const today = new Date();
-                    const beginningOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-                    let newEndTime = 0
-
-                    if (endTime !== 0 && endTime <= today.getTime()) {
-                        newEndTime = input + beginningOfDay.getTime() + 24 * 60 * 60 * 1000
-                    } else {
-                        newEndTime = input + beginningOfDay.getTime()
-                    }
-
-                    setEndTime(newEndTime)
-                }}
-            />
-            {`Time until alarm: ${convertMillisecondsToClockText(remainingTime)}`}
-            <button onClick={toggleAlarmActivation}
-            >{buttonText}</button>
-            <button onClick={deleteAlarm}>Delete</button>
-        </div >
-    )
+  return (
+    <div>
+      {`Alarm ${id}`}
+      <TimePicker setTime={setAlarmTime} stopTimer={deactivateAlarm} />
+      {`Time until alarm: ${convertMillisecondsToClockTextWithLetters(remainingTime)}`}
+      <button onClick={toggleAlarmActivation}>{buttonText}</button>
+      <button onClick={deleteAlarm}>Delete</button>
+    </div >
+  )
 }
 
 export function AlarmPage({ className }: { className: string }) {
-    const [alarmIds, setAlarmIds] = useState([1]);
+  const [alarmIds, setAlarmIds] = useState([1]);
 
-    const alarms = alarmIds.map((id) =>
-        <Alarm
-            id={id}
-            key={id}
-            deleteAlarm={() => setAlarmIds(deleteIdFromArray(id, alarmIds))}
-        />
-    );
+  const alarms = alarmIds.map((id) =>
+    <Alarm
+      id={id}
+      key={id}
+      deleteAlarm={() => setAlarmIds(deleteIdFromArray(id, alarmIds))}
+    />
+  );
 
-    return (
-        <div className={className}>
-            <button onClick={() => setAlarmIds(addIdToArray(alarmIds))}>Add Alarm</button>
-            {alarms}
-        </div>
-    )
+  return (
+    <div className={className}>
+      <button onClick={() => setAlarmIds(addIdToArray(alarmIds))}>Add Alarm</button>
+      {alarms}
+    </div>
+  )
 }
--- components/common-components.tsx
+++ components/common-components.tsx
@@ -1,30 +1,34 @@
-import { useState } from "react";
+import { KeyboardEvent, useState } from "react";
 import { handleTimeInput, convertClockTextToTime } from "@/functions/general";
 
-export function TimePicker({ setTime, }: { setTime: Function }) {
-    const [userInput, setUserInput] = useState("000000");
-    const timerInputs = ["hours", "minutes", "seconds"].map((value, index) => {
-        return (
-            <div key={index} className="inline">
-                <input
-                    readOnly
-                    value={userInput.slice(index * 2, index * 2 + 2)}
-                    className="text-black"
-                    type="text"
-                    onKeyDown={(e) => {
-                        let newInput = handleTimeInput(e, value, userInput);
-                        setUserInput(newInput);
-                        setTime(convertClockTextToTime(newInput));
-                    }}
-                />
-                <text>{value[0]}</text>
-            </div>
-        )
-    })
+export function TimePicker({ setTime, stopTimer = () => null }: { setTime: (input: number) => void, stopTimer?: Function }) {
+  const [userInput, setUserInput] = useState("000000");
 
+  function handleKeyPress(e: KeyboardEvent<HTMLInputElement>, value: string) {
+    stopTimer()
+    let newInput = handleTimeInput(e, value, userInput);
+    setUserInput(newInput);
+    setTime(convertClockTextToTime(newInput));
+  }
+
+  const timerInputs = ["hours", "minutes", "seconds"].map((value, index) => {
     return (
-        <div>
-            {timerInputs}
-        </div>
+      <div key={index} className="inline">
+        <input
+          readOnly
+          value={userInput.slice(index * 2, index * 2 + 2)}
+          className="text-black"
+          type="text"
+          onKeyDown={(e) => handleKeyPress(e, value)}
+        />
+        <text>{value[0]}</text>
+      </div>
     )
+  })
+
+  return (
+    <div>
+      {timerInputs}
+    </div>
+  )
 }
